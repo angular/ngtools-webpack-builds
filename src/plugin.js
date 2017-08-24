410b94e201268c47c5aae1748d136d2fc1124cca
Travis CI User <travis@example.org>
2017-08-24 01:43:41 +0000

    6713e76 fix(@angular/cli): Allow patch updates to @schematics/angular (#7483)

--- src/plugin.js
+++ src/plugin.js
@@ -286,11 +286,8 @@ class AotPlugin {
         });
         compiler.plugin('after-resolvers', (compiler) => {
             // Virtual file system.
-            // Wait for the plugin to be done when requesting `.ts` files directly (entry points), or
-            // when the issuer is a `.ts` file.
             compiler.resolvers.normal.plugin('before-resolve', (request, cb) => {
-                if (request.request.endsWith('.ts')
-                    || (request.context.issuer && request.context.issuer.endsWith('.ts'))) {
+                if (request.request.match(/\.ts$/)) {
                     this.done.then(() => cb(), () => cb());
                 }
                 else {
