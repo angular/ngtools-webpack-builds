f0710eb4a64eac509b9c9aed86e823eb7e1228cc
Travis CI User <travis@example.org>
2017-09-14 01:42:46 +0000

    b0fd35e feat(@angular/cli): add --preserve-symlinks to ng test

--- src/transformers/export_ngfactory.d.ts
+++
@@ -1,6 +0,0 @@
-import * as ts from 'typescript';
-import { TransformOperation } from './make_transform';
-export declare function exportNgFactory(sourceFile: ts.SourceFile, entryModule: {
-    path: string;
-    className: string;
-}): TransformOperation[];
