81281fb15f961cc7adc20e92b8b76bdfe601e6ae
Travis CI User <travis@example.org>
2018-01-18 19:04:58 +0000

    0e531e7 release: 1.7.0-beta.0

--- src/transformers/elide_imports.js
+++ src/transformers/elide_imports.js
@@ -68,10 +68,7 @@ function elideImports(sourceFile, removedNodes, getTypeChecker) {
         .forEach((id) => {
         if (removedSymbolMap.has(id.text)) {
             const symbol = removedSymbolMap.get(id.text);
-            // Check if the symbol is the same or if it is a named export.
-            // Named exports don't have the same symbol but will have the same name.
-            if ((id.parent && id.parent.kind === ts.SyntaxKind.ExportSpecifier)
-                || typeChecker.getSymbolAtLocation(id) === symbol.symbol) {
+            if (typeChecker.getSymbolAtLocation(id) === symbol.symbol) {
                 symbol.all.push(id);
             }
         }
