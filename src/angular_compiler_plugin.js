bbc776e601d6513f1dc65c637bbdbc2987eca37c
Travis CI User <travis@example.org>
2018-02-01 01:21:09 +0000

    672768b build: limit typescript version to patches

--- src/angular_compiler_plugin.js
+++ src/angular_compiler_plugin.js
@@ -95,7 +95,6 @@ class AngularCompilerPlugin {
         this._basePath = config.options.basePath;
         // Overwrite outDir so we can find generated files next to their .ts origin in compilerHost.
         this._compilerOptions.outDir = '';
-        this._compilerOptions.suppressOutputPathCheck = true;
         // Default plugin sourceMap to compiler options setting.
         if (!options.hasOwnProperty('sourceMap')) {
             options.sourceMap = this._compilerOptions.sourceMap || false;
