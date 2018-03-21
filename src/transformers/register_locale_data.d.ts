44ab354de7b5f9e2737f4705d6b61700cac726cb
Angular Builds <circleci@angular.io>
2018-03-21 13:08:51 +0000

    d7e7b95 test: fix ngtools/webpack local-bootstrap resolution

--- src/transformers/register_locale_data.d.ts
+++ src/transformers/register_locale_data.d.ts
@@ -1,5 +1,12 @@
+/**
+ * @license
+ * Copyright Google Inc. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
 import * as ts from 'typescript';
 export declare function registerLocaleData(shouldTransform: (fileName: string) => boolean, getEntryModule: () => {
     path: string;
     className: string;
-}, locale: string): ts.TransformerFactory<ts.SourceFile>;
+} | null, locale: string): ts.TransformerFactory<ts.SourceFile>;
