99e08151639ff56613c1bfb8abce7f6f8fad6939
Angular Builds <circleci@angular.io>
2018-03-23 14:35:33 +0000

    4881493 feat(@schematics/angular): add a default browserslist file

--- src/webpack.d.ts
+++ src/webpack.d.ts
@@ -1,4 +1,11 @@
 /// <reference types="node" />
+/**
+ * @license
+ * Copyright Google Inc. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
 import { Stats } from 'fs';
 export interface Request {
     request?: Request;
@@ -31,15 +38,15 @@ export interface NormalModuleFactoryRequest {
     };
 }
 export interface InputFileSystem {
-    stat(path: string, callback: Callback<any>): void;
-    readdir(path: string, callback: Callback<any>): void;
-    readFile(path: string, callback: Callback<any>): void;
+    stat(path: string, callback: Callback<Stats>): void;
+    readdir(path: string, callback: Callback<string[]>): void;
+    readFile(path: string, callback: Callback<string>): void;
     readJson(path: string, callback: Callback<any>): void;
-    readlink(path: string, callback: Callback<any>): void;
+    readlink(path: string, callback: Callback<string>): void;
     statSync(path: string): Stats;
     readdirSync(path: string): string[];
     readFileSync(path: string): string;
-    readJsonSync(path: string): string;
+    readJsonSync(path: string): any;
     readlinkSync(path: string): string;
     purge(changes?: string[] | string): void;
 }
