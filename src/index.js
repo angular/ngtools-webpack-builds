0838fcf0350adbe57d073caa7e60fbf02dc4c779
Travis CI User <travis@example.org>
2018-04-05 20:31:44 +0000

    e48d545 fix(@angular/cli): disables clean-css mergeIntoShorthands

--- src/index.js
+++ src/index.js
@@ -1,16 +1,29 @@
 "use strict";
-/**
- * @license
- * Copyright Google Inc. All Rights Reserved.
- *
- * Use of this source code is governed by an MIT-style license that can be
- * found in the LICENSE file at https://angular.io/license
- */
 function __export(m) {
     for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
 }
 Object.defineProperty(exports, "__esModule", { value: true });
+// @ignoreDep typescript
+const semver_1 = require("semver");
+// Test if typescript is available. This is a hack. We should be using peerDependencies instead
+// but can't until we split global and local packages.
+// See https://github.com/angular/angular-cli/issues/8107#issuecomment-338185872
+try {
+    const version = require('typescript').version;
+    if (!semver_1.gte(version, '2.0.2')) {
+        throw new Error();
+    }
+}
+catch (e) {
+    throw new Error('Could not find local "typescript" package.'
+        + 'The "@ngtools/webpack" package requires a local "typescript@^2.0.2" package to be installed.'
+        + e);
+}
+__export(require("./plugin"));
 __export(require("./angular_compiler_plugin"));
+__export(require("./extract_i18n_plugin"));
 var loader_1 = require("./loader");
 exports.default = loader_1.ngcLoader;
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VSb290IjoiLi8iLCJzb3VyY2VzIjpbInBhY2thZ2VzL25ndG9vbHMvd2VicGFjay9zcmMvaW5kZXgudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IjtBQUFBOzs7Ozs7R0FNRzs7Ozs7QUFFSCwrQ0FBMEM7QUFDMUMsbUNBQWdEO0FBQXZDLDJCQUFBLFNBQVMsQ0FBVyIsInNvdXJjZXNDb250ZW50IjpbIi8qKlxuICogQGxpY2Vuc2VcbiAqIENvcHlyaWdodCBHb29nbGUgSW5jLiBBbGwgUmlnaHRzIFJlc2VydmVkLlxuICpcbiAqIFVzZSBvZiB0aGlzIHNvdXJjZSBjb2RlIGlzIGdvdmVybmVkIGJ5IGFuIE1JVC1zdHlsZSBsaWNlbnNlIHRoYXQgY2FuIGJlXG4gKiBmb3VuZCBpbiB0aGUgTElDRU5TRSBmaWxlIGF0IGh0dHBzOi8vYW5ndWxhci5pby9saWNlbnNlXG4gKi9cblxuZXhwb3J0ICogZnJvbSAnLi9hbmd1bGFyX2NvbXBpbGVyX3BsdWdpbic7XG5leHBvcnQgeyBuZ2NMb2FkZXIgYXMgZGVmYXVsdCB9IGZyb20gJy4vbG9hZGVyJztcbiJdfQ==
+var paths_plugin_1 = require("./paths-plugin");
+exports.PathsPlugin = paths_plugin_1.PathsPlugin;
+//# sourceMappingURL=/home/travis/build/angular/angular-cli/src/index.js.map
