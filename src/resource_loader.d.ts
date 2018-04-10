c36cb8a1921de0df215707fb4671c1ad04956cf2
Angular Builds <circleci@angular.io>
2018-04-10 14:27:33 +0000

    86aff35 test(@angular-devkit/build-angular): re-enable karma es2015 test

--- src/resource_loader.d.ts
+++ src/resource_loader.d.ts
@@ -1,8 +1,9 @@
 export declare class WebpackResourceLoader {
     private _parentCompilation;
     private _context;
-    private _resourceDependencies;
-    private _cachedResources;
+    private _fileDependencies;
+    private _cachedSources;
+    private _cachedEvaluatedSources;
     constructor();
     update(parentCompilation: any): void;
     getResourceDependencies(filePath: string): string[];
