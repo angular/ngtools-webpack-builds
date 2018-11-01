266a43720b997bb96b253357bd3d780551afc49f
Angular Builds <circleci@angular.io>
2018-11-01 17:49:55 +0000

    0909943 test(@angular-devkit/build-angular): test karma on watch mode

--- src/gather_diagnostics.d.ts
+++ src/gather_diagnostics.d.ts
@@ -7,6 +7,12 @@
  */
 import { Diagnostics, Program } from '@angular/compiler-cli';
 import * as ts from 'typescript';
+export declare enum DiagnosticMode {
+    Syntactic = 1,
+    Semantic = 2,
+    All = 3,
+    Default = 3
+}
 export declare class CancellationToken implements ts.CancellationToken {
     private _isCancelled;
     requestCancellation(): void;
@@ -14,4 +20,4 @@ export declare class CancellationToken implements ts.CancellationToken {
     throwIfCancellationRequested(): void;
 }
 export declare function hasErrors(diags: Diagnostics): boolean;
-export declare function gatherDiagnostics(program: ts.Program | Program, jitMode: boolean, benchmarkLabel: string, cancellationToken?: CancellationToken): Diagnostics;
+export declare function gatherDiagnostics(program: ts.Program | Program, jitMode: boolean, benchmarkLabel: string, mode?: DiagnosticMode, cancellationToken?: CancellationToken): Diagnostics;
