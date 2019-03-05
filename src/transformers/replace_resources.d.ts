72362c75093da2c926ba182f5071d742ff1fb3e3
Angular Builds <circleci@angular.io>
2019-03-05 18:37:07 +0000

    dfb08b9 feat(@ngtools/webpack): allow .svg files as templates

--- src/transformers/replace_resources.d.ts
+++ src/transformers/replace_resources.d.ts
@@ -6,5 +6,5 @@
  * found in the LICENSE file at https://angular.io/license
  */
 import * as ts from 'typescript';
-export declare function replaceResources(shouldTransform: (fileName: string) => boolean, getTypeChecker: () => ts.TypeChecker): ts.TransformerFactory<ts.SourceFile>;
-export declare function getResourceUrl(node: ts.Expression): string | null;
+export declare function replaceResources(shouldTransform: (fileName: string) => boolean, getTypeChecker: () => ts.TypeChecker, directTemplateLoading?: boolean): ts.TransformerFactory<ts.SourceFile>;
+export declare function getResourceUrl(node: ts.Expression, loader?: string): string | null;
