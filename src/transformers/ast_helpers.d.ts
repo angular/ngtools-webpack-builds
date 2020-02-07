1eb77d7ec94fbd1010d58783bf93ca2261eecedf
Angular Builds <circleci@angular.io>
2020-02-07 01:30:52 +0000

    207b2ccc5 release: v9.0.1

--- src/transformers/ast_helpers.d.ts
+++ src/transformers/ast_helpers.d.ts
@@ -1,10 +1,11 @@
+/**
+ * @license
+ * Copyright Google Inc. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
 import * as ts from 'typescript';
-import { WebpackCompilerHost } from '../compiler_host';
 export declare function collectDeepNodes<T extends ts.Node>(node: ts.Node, kind: ts.SyntaxKind | ts.SyntaxKind[]): T[];
 export declare function getFirstNode(sourceFile: ts.SourceFile): ts.Node;
 export declare function getLastNode(sourceFile: ts.SourceFile): ts.Node | null;
-export declare function createTypescriptContext(content: string, additionalFiles?: Record<string, string>, useLibs?: boolean, extraCompilerOptions?: ts.CompilerOptions): {
-    compilerHost: WebpackCompilerHost;
-    program: ts.Program;
-};
-export declare function transformTypescript(content: string | undefined, transformers: ts.TransformerFactory<ts.SourceFile>[], program?: ts.Program, compilerHost?: WebpackCompilerHost): string | undefined;
