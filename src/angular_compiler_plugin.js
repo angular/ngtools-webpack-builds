6136a8fab2f62193e9b71a36ca0553e0fc993172
Angular Builds <circleci@angular.io>
2020-06-10 19:30:04 +0000

    f15ec1945 fix(@schematics/angular): specify module option as a fix for the multiple module error

--- src/angular_compiler_plugin.js
+++ src/angular_compiler_plugin.js
@@ -568,7 +568,7 @@ class AngularCompilerPlugin {
             }
             let ngccProcessor;
             if (this._compilerOptions.enableIvy) {
-                ngccProcessor = new ngcc_processor_1.NgccProcessor(this._mainFields, compilerWithFileSystems.inputFileSystem, this._warnings, this._errors, this._basePath, this._compilerOptions, this._tsConfigPath);
+                ngccProcessor = new ngcc_processor_1.NgccProcessor(this._mainFields, compilerWithFileSystems.inputFileSystem, this._warnings, this._errors, this._basePath, this._tsConfigPath);
                 ngccProcessor.process();
             }
             // Use an identity function as all our paths are absolute already.
--- src/ngcc_processor.d.ts
+++ src/ngcc_processor.d.ts
@@ -13,13 +13,11 @@ export declare class NgccProcessor {
     private readonly compilationWarnings;
     private readonly compilationErrors;
     private readonly basePath;
-    private readonly compilerOptions;
     private readonly tsConfigPath;
     private _processedModules;
     private _logger;
     private _nodeModulesDirectory;
-    private _pathMappings;
-    constructor(propertiesToConsider: string[], inputFileSystem: InputFileSystem, compilationWarnings: (Error | string)[], compilationErrors: (Error | string)[], basePath: string, compilerOptions: ts.CompilerOptions, tsConfigPath: string);
+    constructor(propertiesToConsider: string[], inputFileSystem: InputFileSystem, compilationWarnings: (Error | string)[], compilationErrors: (Error | string)[], basePath: string, tsConfigPath: string);
     /** Process the entire node modules tree. */
     process(): void;
     /** Process a module and it's depedencies. */
