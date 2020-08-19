72dd71955db323660818de8e9c1483e0d91977ad
Angular Builds <circleci@angular.io>
2020-08-19 14:34:42 +0000

    1be09a03d ci: change bazel to use XL container resources

--- src/webpack-input-host.d.ts
+++ src/webpack-input-host.d.ts
@@ -6,7 +6,22 @@
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-import { virtualFs } from '@angular-devkit/core';
+import { Path, PathFragment, virtualFs } from '@angular-devkit/core';
 import { Stats } from 'fs';
+import { Observable } from 'rxjs';
 import { InputFileSystem } from 'webpack';
-export declare function createWebpackInputHost(inputFileSystem: InputFileSystem): virtualFs.Host<Stats>;
+export declare class WebpackInputHost implements virtualFs.Host<Stats> {
+    readonly inputFileSystem: InputFileSystem;
+    constructor(inputFileSystem: InputFileSystem);
+    get capabilities(): virtualFs.HostCapabilities;
+    write(_path: Path, _content: virtualFs.FileBufferLike): Observable<never>;
+    delete(_path: Path): Observable<never>;
+    rename(_from: Path, _to: Path): Observable<never>;
+    read(path: Path): Observable<virtualFs.FileBuffer>;
+    list(path: Path): Observable<PathFragment[]>;
+    exists(path: Path): Observable<boolean>;
+    isDirectory(path: Path): Observable<boolean>;
+    isFile(path: Path): Observable<boolean>;
+    stat(path: Path): Observable<Stats | null>;
+    watch(_path: Path, _options?: virtualFs.HostWatchOptions): null;
+}
--- src/webpack-input-host.js
+++ src/webpack-input-host.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.createWebpackInputHost = void 0;
+exports.WebpackInputHost = void 0;
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
@@ -9,49 +9,83 @@ exports.createWebpackInputHost = void 0;
  * found in the LICENSE file at https://angular.io/license
  */
 const core_1 = require("@angular-devkit/core");
+const rxjs_1 = require("rxjs");
+const operators_1 = require("rxjs/operators");
 // Host is used instead of ReadonlyHost due to most decorators only supporting Hosts
-function createWebpackInputHost(inputFileSystem) {
-    return core_1.virtualFs.createSyncHost({
-        write() {
-            throw new Error('Not supported.');
-        },
-        delete() {
-            throw new Error('Not supported.');
-        },
-        rename() {
-            throw new Error('Not supported.');
-        },
-        read(path) {
-            const data = inputFileSystem.readFileSync(core_1.getSystemPath(path));
-            return new Uint8Array(data).buffer;
-        },
-        list(path) {
-            // readdirSync exists but is not in the Webpack typings
-            const names = inputFileSystem.readdirSync(core_1.getSystemPath(path));
-            return names.map((name) => core_1.fragment(name));
-        },
-        exists(path) {
-            return !!this.stat(path);
-        },
-        isDirectory(path) {
-            var _a, _b;
-            return (_b = (_a = this.stat(path)) === null || _a === void 0 ? void 0 : _a.isDirectory()) !== null && _b !== void 0 ? _b : false;
-        },
-        isFile(path) {
-            var _a, _b;
-            return (_b = (_a = this.stat(path)) === null || _a === void 0 ? void 0 : _a.isFile()) !== null && _b !== void 0 ? _b : false;
-        },
-        stat(path) {
+class WebpackInputHost {
+    constructor(inputFileSystem) {
+        this.inputFileSystem = inputFileSystem;
+    }
+    get capabilities() {
+        return { synchronous: true };
+    }
+    write(_path, _content) {
+        return rxjs_1.throwError(new Error('Not supported.'));
+    }
+    delete(_path) {
+        return rxjs_1.throwError(new Error('Not supported.'));
+    }
+    rename(_from, _to) {
+        return rxjs_1.throwError(new Error('Not supported.'));
+    }
+    read(path) {
+        return new rxjs_1.Observable(obs => {
+            // TODO: remove this try+catch when issue https://github.com/ReactiveX/rxjs/issues/3740 is
+            // fixed.
             try {
-                return inputFileSystem.statSync(core_1.getSystemPath(path));
+                const data = this.inputFileSystem.readFileSync(core_1.getSystemPath(path));
+                obs.next(new Uint8Array(data).buffer);
+                obs.complete();
+            }
+            catch (e) {
+                obs.error(e);
+            }
+        });
+    }
+    list(path) {
+        return new rxjs_1.Observable(obs => {
+            // TODO: remove this try+catch when issue https://github.com/ReactiveX/rxjs/issues/3740 is
+            // fixed.
+            try {
+                // tslint:disable-next-line:no-any
+                const names = this.inputFileSystem.readdirSync(core_1.getSystemPath(path));
+                obs.next(names.map(name => core_1.fragment(name)));
+                obs.complete();
+            }
+            catch (err) {
+                obs.error(err);
+            }
+        });
+    }
+    exists(path) {
+        return this.stat(path).pipe(operators_1.map(stats => stats != null));
+    }
+    isDirectory(path) {
+        return this.stat(path).pipe(operators_1.map(stats => stats != null && stats.isDirectory()));
+    }
+    isFile(path) {
+        return this.stat(path).pipe(operators_1.map(stats => stats != null && stats.isFile()));
+    }
+    stat(path) {
+        return new rxjs_1.Observable(obs => {
+            try {
+                const stats = this.inputFileSystem.statSync(core_1.getSystemPath(path));
+                obs.next(stats);
+                obs.complete();
             }
             catch (e) {
                 if (e.code === 'ENOENT') {
-                    return null;
+                    obs.next(null);
+                    obs.complete();
                 }
-                throw e;
+                else {
+                    obs.error(e);
+                }
             }
-        },
-    });
+        });
+    }
+    watch(_path, _options) {
+        return null;
+    }
 }
-exports.createWebpackInputHost = createWebpackInputHost;
+exports.WebpackInputHost = WebpackInputHost;
