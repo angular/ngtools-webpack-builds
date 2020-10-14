5b9ac2c13dfecdda31e42aeed9b85d95221a1c97
Angular Builds <circleci@angular.io>
2020-10-14 14:46:18 +0000

    83afc6307 test(@angular/cli): update Webpack 5 E2E test to 5.1.0

--- src/webpack-version.d.ts
+++ src/webpack-version.d.ts
@@ -1,1 +1,2 @@
 export declare function isWebpackFiveOrHigher(): boolean;
+export declare function mergeResolverMainFields(options: object, originalMainFields: string[], extraMainFields: string[]): object;
