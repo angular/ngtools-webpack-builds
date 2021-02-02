b75237bc624a8bdca9415b108aff7b451d7c81b8
Angular Builds <circleci@angular.io>
2021-02-02 09:10:14 +0000

    3aaf6a49f build: update pacote to version 11.2.4

--- src/resource_loader.d.ts
+++ src/resource_loader.d.ts
@@ -1,11 +1,9 @@
 export declare class WebpackResourceLoader {
     private _parentCompilation;
-    private _context;
     private _fileDependencies;
     private _reverseDependencies;
-    private _cachedSources;
-    private _cachedEvaluatedSources;
-    changedFiles?: Iterable<string>;
+    private cache;
+    private modifiedResources;
     update(parentCompilation: import('webpack').compilation.Compilation, changedFiles?: Iterable<string>): void;
     getModifiedResourceFiles(): Set<string>;
     getResourceDependencies(filePath: string): never[] | Set<string>;
