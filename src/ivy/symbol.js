3ccf4e3d7eff534f3a5ccc5aede1f49ef899bbf4
Angular Builds <circleci@angular.io>
2021-06-10 16:13:29 +0000

    559a65c51 build: update Angular dependencies to 12.1 prerelease versions

--- src/ivy/symbol.js
+++ src/ivy/symbol.js
@@ -6,53 +6,51 @@
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var __classPrivateFieldSet = (this && this.__classPrivateFieldSet) || function (receiver, privateMap, value) {
-    if (!privateMap.has(receiver)) {
-        throw new TypeError("attempted to set private field on non-instance");
-    }
-    privateMap.set(receiver, value);
-    return value;
+var __classPrivateFieldSet = (this && this.__classPrivateFieldSet) || function (receiver, state, value, kind, f) {
+    if (kind === "m") throw new TypeError("Private method is not writable");
+    if (kind === "a" && !f) throw new TypeError("Private accessor was defined without a setter");
+    if (typeof state === "function" ? receiver !== state || !f : !state.has(receiver)) throw new TypeError("Cannot write private member to an object whose class did not declare it");
+    return (kind === "a" ? f.call(receiver, value) : f ? f.value = value : state.set(receiver, value)), value;
 };
-var __classPrivateFieldGet = (this && this.__classPrivateFieldGet) || function (receiver, privateMap) {
-    if (!privateMap.has(receiver)) {
-        throw new TypeError("attempted to get private field on non-instance");
-    }
-    return privateMap.get(receiver);
+var __classPrivateFieldGet = (this && this.__classPrivateFieldGet) || function (receiver, state, kind, f) {
+    if (kind === "a" && !f) throw new TypeError("Private accessor was defined without a getter");
+    if (typeof state === "function" ? receiver !== state || !f : !state.has(receiver)) throw new TypeError("Cannot read private member from an object whose class did not declare it");
+    return kind === "m" ? f : kind === "a" ? f.call(receiver) : f ? f.value : state.get(receiver);
 };
-var _fileEmitter, _registrations;
+var _FileEmitterRegistration_fileEmitter, _FileEmitterCollection_registrations;
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.FileEmitterCollection = exports.FileEmitterRegistration = exports.AngularPluginSymbol = void 0;
 exports.AngularPluginSymbol = Symbol.for('@angular-devkit/build-angular[angular-compiler]');
 class FileEmitterRegistration {
     constructor() {
-        _fileEmitter.set(this, void 0);
+        _FileEmitterRegistration_fileEmitter.set(this, void 0);
     }
     update(emitter) {
-        __classPrivateFieldSet(this, _fileEmitter, emitter);
+        __classPrivateFieldSet(this, _FileEmitterRegistration_fileEmitter, emitter, "f");
     }
     emit(file) {
-        if (!__classPrivateFieldGet(this, _fileEmitter)) {
+        if (!__classPrivateFieldGet(this, _FileEmitterRegistration_fileEmitter, "f")) {
             throw new Error('Emit attempted before Angular Webpack plugin initialization.');
         }
-        return __classPrivateFieldGet(this, _fileEmitter).call(this, file);
+        return __classPrivateFieldGet(this, _FileEmitterRegistration_fileEmitter, "f").call(this, file);
     }
 }
 exports.FileEmitterRegistration = FileEmitterRegistration;
-_fileEmitter = new WeakMap();
+_FileEmitterRegistration_fileEmitter = new WeakMap();
 class FileEmitterCollection {
     constructor() {
-        _registrations.set(this, []);
+        _FileEmitterCollection_registrations.set(this, []);
     }
     register() {
         const registration = new FileEmitterRegistration();
-        __classPrivateFieldGet(this, _registrations).push(registration);
+        __classPrivateFieldGet(this, _FileEmitterCollection_registrations, "f").push(registration);
         return registration;
     }
     async emit(file) {
-        if (__classPrivateFieldGet(this, _registrations).length === 1) {
-            return __classPrivateFieldGet(this, _registrations)[0].emit(file);
+        if (__classPrivateFieldGet(this, _FileEmitterCollection_registrations, "f").length === 1) {
+            return __classPrivateFieldGet(this, _FileEmitterCollection_registrations, "f")[0].emit(file);
         }
-        for (const registration of __classPrivateFieldGet(this, _registrations)) {
+        for (const registration of __classPrivateFieldGet(this, _FileEmitterCollection_registrations, "f")) {
             const result = await registration.emit(file);
             if (result) {
                 return result;
@@ -61,4 +59,4 @@ class FileEmitterCollection {
     }
 }
 exports.FileEmitterCollection = FileEmitterCollection;
-_registrations = new WeakMap();
+_FileEmitterCollection_registrations = new WeakMap();
