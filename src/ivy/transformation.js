268823a9a7380f33421ed309f5e2c30250aaf3ed
Angular Builds <circleci@angular.io>
2021-06-13 16:36:32 +0000

    b3d708014 build: enable `esModuleInterop` TypeScript option

--- src/ivy/transformation.js
+++ src/ivy/transformation.js
@@ -6,10 +6,29 @@
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.replaceBootstrap = exports.mergeTransformers = exports.createJitTransformers = exports.createAotTransformers = void 0;
 const compiler_cli_1 = require("@angular/compiler-cli");
-const ts = require("typescript");
+const ts = __importStar(require("typescript"));
 const elide_imports_1 = require("../transformers/elide_imports");
 const remove_ivy_jit_support_calls_1 = require("../transformers/remove-ivy-jit-support-calls");
 const replace_resources_1 = require("../transformers/replace_resources");
