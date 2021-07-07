14eddff47b97614f06b5b53dd150573bccf10a1c
Angular Builds <circleci@angular.io>
2021-07-07 16:32:38 +0000

    492c879cd refactor(@ngtools/webpack): consolidate diagnostic helper functions

--- src/ivy/diagnostics.d.ts
+++ src/ivy/diagnostics.d.ts
@@ -6,6 +6,8 @@
  * found in the LICENSE file at https://angular.io/license
  */
 import { Diagnostics } from '@angular/compiler-cli';
-import { Compilation } from 'webpack';
+import type { Compilation } from 'webpack';
 export declare type DiagnosticsReporter = (diagnostics: Diagnostics) => void;
 export declare function createDiagnosticsReporter(compilation: Compilation): DiagnosticsReporter;
+export declare function addWarning(compilation: Compilation, message: string): void;
+export declare function addError(compilation: Compilation, message: string): void;
--- src/ivy/diagnostics.js
+++ src/ivy/diagnostics.js
@@ -7,21 +7,28 @@
  * found in the LICENSE file at https://angular.io/license
  */
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.createDiagnosticsReporter = void 0;
+exports.addError = exports.addWarning = exports.createDiagnosticsReporter = void 0;
 const compiler_cli_1 = require("@angular/compiler-cli");
 const typescript_1 = require("typescript");
-const webpack_diagnostics_1 = require("../webpack-diagnostics");
 function createDiagnosticsReporter(compilation) {
     return (diagnostics) => {
         for (const diagnostic of diagnostics) {
             const text = compiler_cli_1.formatDiagnostics([diagnostic]);
             if (diagnostic.category === typescript_1.DiagnosticCategory.Error) {
-                webpack_diagnostics_1.addError(compilation, text);
+                addError(compilation, text);
             }
             else {
-                webpack_diagnostics_1.addWarning(compilation, text);
+                addWarning(compilation, text);
             }
         }
     };
 }
 exports.createDiagnosticsReporter = createDiagnosticsReporter;
+function addWarning(compilation, message) {
+    compilation.warnings.push(new compilation.compiler.webpack.WebpackError(message));
+}
+exports.addWarning = addWarning;
+function addError(compilation, message) {
+    compilation.errors.push(new compilation.compiler.webpack.WebpackError(message));
+}
+exports.addError = addError;
