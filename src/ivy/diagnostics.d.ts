5f4309fe9f474a2d97d5786a86f3253975a93d63
Angular Builds <circleci@angular.io>
2021-07-07 16:33:58 +0000

    beb78ef79 refactor(@angular-devkit/build-angular): use Webpack provided watcher typings instead of custom

--- src/ivy/diagnostics.d.ts
+++ src/ivy/diagnostics.d.ts
@@ -6,8 +6,6 @@
  * found in the LICENSE file at https://angular.io/license
  */
 import { Diagnostics } from '@angular/compiler-cli';
-import type { Compilation } from 'webpack';
+import { Compilation } from 'webpack';
 export declare type DiagnosticsReporter = (diagnostics: Diagnostics) => void;
 export declare function createDiagnosticsReporter(compilation: Compilation): DiagnosticsReporter;
-export declare function addWarning(compilation: Compilation, message: string): void;
-export declare function addError(compilation: Compilation, message: string): void;
