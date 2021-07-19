d2c8ac2f416cdf398b7d111898cad7fa5c01bd1a
Angular Builds <circleci@angular.io>
2021-07-19 05:46:21 +0000

    cb1db78f1 build: update all non-major dependencies

--- src/ivy/loader.d.ts
+++ src/ivy/loader.d.ts
@@ -5,6 +5,8 @@
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-import type { LoaderContext } from 'webpack';
-export declare function angularWebpackLoader(this: LoaderContext<unknown>, content: string, map: string): void;
+import { AngularPluginSymbol, FileEmitterCollection } from './symbol';
+export declare function angularWebpackLoader(this: import('webpack').LoaderContext<unknown> & {
+    [AngularPluginSymbol]?: FileEmitterCollection;
+}, content: string, map: string): void;
 export { angularWebpackLoader as default };
