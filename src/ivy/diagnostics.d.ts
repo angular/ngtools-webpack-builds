7d92c027aa2df3529b948377c545b30295b2e5a6
Angular Builds <circleci@angular.io>
2021-12-10 12:31:45 +0000

    28924d227 build: update dependency puppeteer to v13

--- src/ivy/diagnostics.d.ts
+++ src/ivy/diagnostics.d.ts
@@ -5,9 +5,9 @@
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-import type { Diagnostics } from '@angular/compiler-cli';
+import { Diagnostic } from 'typescript';
 import type { Compilation } from 'webpack';
-export declare type DiagnosticsReporter = (diagnostics: Diagnostics) => void;
-export declare function createDiagnosticsReporter(compilation: Compilation, formatter: (diagnostic: Diagnostics[number]) => string): DiagnosticsReporter;
+export declare type DiagnosticsReporter = (diagnostics: readonly Diagnostic[]) => void;
+export declare function createDiagnosticsReporter(compilation: Compilation, formatter: (diagnostic: Diagnostic) => string): DiagnosticsReporter;
 export declare function addWarning(compilation: Compilation, message: string): void;
 export declare function addError(compilation: Compilation, message: string): void;
