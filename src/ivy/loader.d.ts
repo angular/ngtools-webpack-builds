933d10c320c90b9f5c3765a1517662e636c2e040
Angular Builds <circleci@angular.dev>
2024-06-06 09:16:51 +0000

    27cb531 refactor: format bazel build files

--- src/ivy/loader.d.ts
+++ src/ivy/loader.d.ts
@@ -3,7 +3,7 @@
  * Copyright Google LLC All Rights Reserved.
  *
  * Use of this source code is governed by an MIT-style license that can be
- * found in the LICENSE file at https://angular.io/license
+ * found in the LICENSE file at https://angular.dev/license
  */
 import type { LoaderContext } from 'webpack';
 export declare function angularWebpackLoader(this: LoaderContext<unknown>, content: string, map: string): void;
