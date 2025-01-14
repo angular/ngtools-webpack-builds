c397d52a59052077a09d6948f2a08ab1a3e59d83
Angular Builds <dev-infra@angular.dev>
2025-01-14 15:47:18 +0000

    e7201a8 refactor(@ngtools/webpack): add return types to functions

--- src/transformers/spec_helpers.js
+++ src/transformers/spec_helpers.js
@@ -6,34 +6,14 @@
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.dev/license
  */
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    var desc = Object.getOwnPropertyDescriptor(m, k);
-    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
-      desc = { enumerable: true, get: function() { return m[k]; } };
-    }
-    Object.defineProperty(o, k2, desc);
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
-    Object.defineProperty(o, "default", { enumerable: true, value: v });
-}) : function(o, v) {
-    o["default"] = v;
-});
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
-    __setModuleDefault(result, mod);
-    return result;
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.createTypescriptContext = createTypescriptContext;
 exports.transformTypescript = transformTypescript;
-const path_1 = require("path");
-const ts = __importStar(require("typescript"));
+const node_path_1 = require("node:path");
+const typescript_1 = __importDefault(require("typescript"));
 // Test transform helpers.
 const basefileName = 'test-file.ts';
 function createTypescriptContext(content, additionalFiles, useLibs = false, extraCompilerOptions = {}, jsxFile = false) {
@@ -42,10 +22,10 @@ function createTypescriptContext(content, additionalFiles, useLibs = false, extr
     const compilerOptions = {
         noEmitOnError: useLibs,
         allowJs: true,
-        newLine: ts.NewLineKind.LineFeed,
-        moduleResolution: ts.ModuleResolutionKind.Node10,
-        module: ts.ModuleKind.ES2020,
-        target: ts.ScriptTarget.ES2020,
+        newLine: typescript_1.default.NewLineKind.LineFeed,
+        moduleResolution: typescript_1.default.ModuleResolutionKind.Node10,
+        module: typescript_1.default.ModuleKind.ES2020,
+        target: typescript_1.default.ScriptTarget.ES2020,
         skipLibCheck: true,
         sourceMap: false,
         importHelpers: true,
@@ -54,11 +34,11 @@ function createTypescriptContext(content, additionalFiles, useLibs = false, extr
         ...extraCompilerOptions,
     };
     // Create compiler host.
-    const compilerHost = ts.createCompilerHost(compilerOptions, true);
+    const compilerHost = typescript_1.default.createCompilerHost(compilerOptions, true);
     const baseFileExists = compilerHost.fileExists;
     compilerHost.fileExists = function (compilerFileName) {
         return (compilerFileName === fileName ||
-            !!additionalFiles?.[(0, path_1.basename)(compilerFileName)] ||
+            !!additionalFiles?.[(0, node_path_1.basename)(compilerFileName)] ||
             baseFileExists(compilerFileName));
     };
     const baseReadFile = compilerHost.readFile;
@@ -66,15 +46,15 @@ function createTypescriptContext(content, additionalFiles, useLibs = false, extr
         if (compilerFileName === fileName) {
             return content;
         }
-        else if (additionalFiles?.[(0, path_1.basename)(compilerFileName)]) {
-            return additionalFiles[(0, path_1.basename)(compilerFileName)];
+        else if (additionalFiles?.[(0, node_path_1.basename)(compilerFileName)]) {
+            return additionalFiles[(0, node_path_1.basename)(compilerFileName)];
         }
         else {
             return baseReadFile(compilerFileName);
         }
     };
     // Create the TypeScript program.
-    const program = ts.createProgram([fileName], compilerOptions, compilerHost);
+    const program = typescript_1.default.createProgram([fileName], compilerOptions, compilerHost);
     return { compilerHost, program };
 }
 function transformTypescript(content, transformers, program, compilerHost) {
@@ -101,7 +81,7 @@ function transformTypescript(content, transformers, program, compilerHost) {
     }, undefined, undefined, { before: transformers });
     // Throw error with diagnostics if emit wasn't successfull.
     if (emitSkipped) {
-        throw new Error(ts.formatDiagnostics(diagnostics, compilerHost));
+        throw new Error(typescript_1.default.formatDiagnostics(diagnostics, compilerHost));
     }
     // Return the transpiled js.
     return outputContent;
